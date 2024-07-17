4f681dcb94a9460d5d882cae4730d16be3d05889
DESKTOP-ROETF6N\javastudy <kmdadoo@naver.com>
2024-07-17 17:42:00 +0900

    MustHaveJSP 14장 모델2 파일 게시판 올리기

--- MustHaveJSP/src/main/java/fileupload/FileUtil.java
+++ MustHaveJSP/src/main/java/fileupload/FileUtil.java
@@ -100,33 +100,34 @@ public class FileUtil
 		return listFileName;			
 	}
 	
-	//파일 다운로드
+	// 14-17 파일 다운로드
+	// 명시한 파일을 찾아 다운로드합니다.
 	public static void download(HttpServletRequest req, HttpServletResponse resp,
             String directory, String sfileName, String ofileName) 
 	{
         String sDirectory = req.getServletContext().getRealPath(directory);
         try {
-            // 파일을 찾아 입력 스트림 생성
+        	// 파일을 찾아 입력 스트림 생성
             File file = new File(sDirectory, sfileName);
             InputStream iStream = new FileInputStream(file);
 
             // 한글 파일명 깨짐 방지
-            String client = req.getHeader("User-Agent");
-            if (client.indexOf("WOW64") == -1) {
+            String client = req.getHeader("User-Agent");	// 웹브라우져 종류
+            if (client.indexOf("WOW64") == -1) {	// 익스플로러
                 ofileName = new String(ofileName.getBytes("UTF-8"), "ISO-8859-1");
             }
             else {
                 ofileName = new String(ofileName.getBytes("KSC5601"), "ISO-8859-1");
             }
 
-            // 파일 다운로드용 응답 헤더 설정
+            // 파일 다운로드 응답 헤더 설정
             resp.reset();
             resp.setContentType("application/octet-stream");
             resp.setHeader("Content-Disposition",
                            "attachment; filename=\"" + ofileName + "\"");
             resp.setHeader("Content-Length", "" + file.length() );
 
-            //out.clear();  // 출력 스트림 초기화
+            //out.clear();  // 출력 스트림 초기화 서블릿에서는 예외가 발생하지 않아 주석처리
 
             // response 내장 객체로부터 새로운 출력 스트림 생성
             OutputStream oStream = resp.getOutputStream();
@@ -138,7 +139,7 @@ public class FileUtil
                 oStream.write(b, 0, readBuffer);
             }
 
-            // 입/출력 스트림 닫음
+            // 입출력 스크림 닫음
             iStream.close();
             oStream.close();
         }
@@ -151,14 +152,19 @@ public class FileUtil
             e.printStackTrace();
         }
     }
-
-    //파일 삭제 
-    public static void deleteFile(HttpServletRequest req,
-            String directory, String filename) {
+	
+	// 파일 삭제를 위한 메서드
+	public static void deleteFile(HttpServletRequest req,
+            String directory, String filename) 
+	{
+		// 물리적 경로와 파일명을 통해 File객체를 생성한다.
         String sDirectory = req.getServletContext().getRealPath(directory);
         File file = new File(sDirectory + File.separator + filename);
+        // 해당 경로에 파일이 존재하면...
         if (file.exists()) {
+        	// 파일을 삭제한다.
             file.delete();
         }
     }
+
 }
--- MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
+++ MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
@@ -126,4 +126,102 @@ public class MVCBoardDAO extends DBConnPool	// 커넥션 풀 상속
         }
         return result;
     }
+	
+	// 주어진 일련번호에 해당하는 게시물을 DTO에 담아 반환합니다.
+	public MVCBoardDTO selectView(String idx) {
+        MVCBoardDTO dto = new MVCBoardDTO();  // DTO 객체 생성
+        String query = "SELECT * FROM mvcboard WHERE idx=?";  // 쿼리문 템플릿 준비
+        try {
+            psmt = con.prepareStatement(query);  // 동적 쿼리문 준비
+            psmt.setString(1, idx);  // 인파라미터 설정
+            rs = psmt.executeQuery();  // 쿼리문 실행
+
+            if (rs.next()) {  // 결과를 DTO 객체에 저장
+                dto.setIdx(rs.getString(1));
+                dto.setName(rs.getString(2));
+                dto.setTitle(rs.getString(3));
+                dto.setContent(rs.getString(4));
+                dto.setPostdate(rs.getDate(5));
+                dto.setOfile(rs.getString(6));
+                dto.setSfile(rs.getString(7));
+                dto.setDowncount(rs.getInt(8));
+                dto.setPass(rs.getString(9));
+                dto.setVisitcount(rs.getInt(10));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("게시물 상세보기 중 예외 발생");
+            e.printStackTrace();
+        }
+        return dto;  // 결과 반환
+    }
+
+	// 주어진 일련번호에 해당하는 게시물의 조회수를 1 증가시킵니다.
+    public void updateVisitCount(String idx) {
+        String query = "UPDATE mvcboard SET "
+                     + " visitcount=visitcount+1 "
+                     + " WHERE idx=?"; 
+        try {
+            psmt = con.prepareStatement(query);
+            psmt.setString(1, idx);
+            psmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println("게시물 조회수 증가 중 예외 발생");
+            e.printStackTrace();
+        }
+    }
+    
+    /// 파일 다운로드
+    // 다운로드 횟수를 1 증가시킵니다.
+    public void downCountPlus(String idx) {
+        String sql = "UPDATE mvcboard SET "
+                + " downcount=downcount+1 "
+                + " WHERE idx=? "; 
+        try {
+            psmt = con.prepareStatement(sql);
+            psmt.setString(1, idx);
+            psmt.executeUpdate();
+        }
+        catch (Exception e) {}
+    }
+    
+    // 게시물 삭제
+    // 입력한 비밀번호가 지정한 일련번호의 게시물의 비밀번호와 
+    // 일치하는지 확인합니다.
+    public boolean confirmPassword(String pass, String idx) {
+        boolean isCorr = true;
+        try {
+            String sql = "SELECT COUNT(*) FROM mvcboard WHERE pass=? AND idx=?";
+            psmt = con.prepareStatement(sql);
+            psmt.setString(1, pass);
+            psmt.setString(2, idx);
+            rs = psmt.executeQuery();
+            rs.next();
+            if (rs.getInt(1) == 0) {
+                isCorr = false;
+            }
+        }
+        catch (Exception e) {
+            isCorr = false;
+            e.printStackTrace();
+        }
+        return isCorr;
+    }
+
+    // 지정한 일련번호의 게시물을 삭제합니다.
+    public int deletePost(String idx) {
+        int result = 0;
+        try {
+            String query = "DELETE FROM mvcboard WHERE idx=?";
+            psmt = con.prepareStatement(query);
+            psmt.setString(1, idx);
+            result = psmt.executeUpdate(); //  정상적으로 삭제되면 1을 반환
+        }
+        catch (Exception e) {
+            System.out.println("게시물 삭제 중 예외 발생");
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
